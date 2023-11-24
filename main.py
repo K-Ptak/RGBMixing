1a707bcb9ec6e72258c8ed3f1892b80686cc58e4
K-Ptak <kamil2001p@gmail.com>
2023-11-24 16:30:27 +0100

    Add solution colors to solution box

--- main.py
+++ main.py
@@ -5,14 +5,16 @@ r_button_color = {}
 g_button_color = {}
 b_button_color = {}
 
-def get_button_color(frame, row, col, color_dict):
+
+def get_button_color(row, col, color_dict):
     button_id = f'{color_dict["prefix"]}{row + 1}{col + 1}'
-    return color_dict.get(button_id)  # Use get() to avoid KeyError if button_id not found
+    return color_dict.get(button_id, 'black')  # Default to black if button_id not found
+
 
 def color_mixing(frame, row, col):
-    red_color = get_button_color(frame, row, col, r_button_color)
-    green_color = get_button_color(frame, row, col, g_button_color)
-    blue_color = get_button_color(frame, row, col, b_button_color)
+    red_color = get_button_color(row, col, r_button_color)
+    green_color = get_button_color(row, col, g_button_color)
+    blue_color = get_button_color(row, col, b_button_color)
 
     if red_color == 'black' and green_color == 'black' and blue_color == 'black':
         solution_color = 'black'
@@ -31,15 +33,22 @@ def color_mixing(frame, row, col):
     else:
         solution_color = 'white'
 
+    print(
+        f"r{row + 1},{col + 1}: {red_color}, g{row + 1},{col + 1}: {green_color}, b{row + 1},{col + 1}: {blue_color}, solution: {solution_color}")
 
-    print(f"r{row,col}: {red_color}, g{row,col}: {green_color}, b{row,col}: {blue_color}")
+    # Update solution color on the white grid
+    button_id = f'white{row + 1}{col + 1}'
+    white_grid_button_color[button_id] = solution_color
+    white_grid_button[button_id].configure(bg=solution_color)
 
 
 def create_color_button(frame, color, row, col, color_dict):
     button_id = f'{color[0]}{row + 1}{col + 1}'
     color_dict[button_id] = color
-    button = tk.Button(frame, bg=color, width=1, height=1, command=lambda: toggle_color(frame, button, color, row, col, color_dict))
+    button = tk.Button(frame, bg=color, width=1, height=1,
+                       command=lambda: toggle_color(frame, button, color, row, col, color_dict))
     button.grid(row=row, column=col, sticky='nsew')
+
 
 def toggle_color(frame, button, original_color, row, col, color_dict):
     current_color = button.cget('bg')
@@ -51,6 +60,7 @@ def toggle_color(frame, button, original_color, row, col, color_dict):
 
     color_mixing(frame, row, col)
 
+
 def create_color_boxes(root):
     colors = ['red', 'green', 'blue', 'white']
     num_buttons = 10
@@ -61,9 +71,9 @@ def create_color_boxes(root):
         frame.column, frame.row = 0, 0
 
         if color == 'white':
-            canvas = tk.Canvas(frame, width=num_buttons * 10, height=num_buttons * 10, bg=color)
-            canvas.grid(row=0, column=0, sticky='nsew')
-            create_color_grid(canvas, color, num_buttons * 10, num_buttons, num_buttons)
+            white_frame = tk.Frame(frame)
+            white_frame.grid(row=0, column=0)
+            create_white_grid_buttons(white_frame, num_buttons)
         else:
             color_dict = r_button_color if color == 'red' else g_button_color if color == 'green' else b_button_color
             color_dict["prefix"] = color[0]
@@ -71,16 +81,21 @@ def create_color_boxes(root):
                 for col in range(num_buttons):
                     create_color_button(frame, color, row, col, color_dict)
 
-def create_color_grid(canvas, color, size, rows, columns):
-    square_size = size // columns
-    for row in range(rows):
-        for col in range(columns):
-            x1 = col * square_size
-            y1 = row * square_size
-            x2 = x1 + square_size
-            y2 = y1 + square_size
-            square_id = f'{color[0]}{row + 1}{col + 1}'
-            canvas.create_rectangle(x1, y1, x2, y2, fill=color, outline='black', tags=square_id)
+
+def create_white_grid_buttons(frame, num_buttons):
+    global white_grid_button_color, white_grid_button
+    white_grid_button_color = {}
+    white_grid_button = {}
+
+    for row in range(num_buttons):
+        for col in range(num_buttons):
+            button_id = f'white{row + 1}{col + 1}'
+            white_grid_button_color[button_id] = 'white'
+            button = tk.Button(frame, bg='white', width=1, height=1)
+            button.config(state="disabled")
+            button.grid(row=row, column=col, sticky='nsew')
+            white_grid_button[button_id] = button
+
 
 def main():
     root = tk.Tk()
@@ -89,6 +104,7 @@ def main():
     create_color_boxes(root)
 
     root.mainloop()
+
 
 if __name__ == "__main__":
     main()
